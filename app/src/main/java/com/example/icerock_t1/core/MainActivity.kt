2e4b6b896ba6e0528200ea10ddd52e12e98088f1
Антон Воронкин <nik.norovv@gmail.com>
2022-02-15 14:45:30 +0700

    Верстка auth fragment

--- app/src/main/java/com/example/icerock_t1/core/MainActivity.kt
+++ app/src/main/java/com/example/icerock_t1/core/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.icerock_t1
+package com.example.icerock_t1.core
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.icerock_t1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
